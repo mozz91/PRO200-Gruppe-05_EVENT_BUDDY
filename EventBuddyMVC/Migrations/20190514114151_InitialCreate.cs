13eb3a4e12fa6d4a7d5505452d4a6c20310cf31a
mozz91 <31176967+mozz91@users.noreply.github.com>
2019-05-14 13:56:32 +0200

    Create 20190514114151_InitialCreate.cs

---
+++ EventBuddyMVC/Migrations/20190514114151_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventBuddyMVC.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventBuddy",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventBuddy", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventBuddy");
+        }
+    }
+}
